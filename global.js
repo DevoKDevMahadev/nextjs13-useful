ec718bba95ad2cadf66093997209dcb748342a26
maverick-aala <102719856+maverick-aala@users.noreply.github.com>
2023-12-19 01:55:05 +0530

    pj

--- global.js
+++ global.js
@@ -5,6 +5,12 @@ import "react-summernote/dist/react-summernote.css"; // import styles
 import "summernote/dist/summernote-bs4.css";
 import "summernote/dist/summernote-lite.css"; // Summernote CSS
 
+// import "react-summernote/dist/react-summernote.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import 'jquery/dist/jquery.min.js';
+// import "@/global";
+
 window.jQuery = $;
 
 require("bootstrap");
